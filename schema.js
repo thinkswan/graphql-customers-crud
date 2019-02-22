4bbb837959fc58bcfa6e62ff7833e0e960d05ff2
Graham Swan <thinkswan@gmail.com>
2019-02-22 20:52:48 +0100

    Fetch data from json-server through GraphQL

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -6,13 +7,6 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require('graphql')
-
-// Seed data
-const customers = [
-  { id: '1', name: 'John Doe', email: 'jdoe@example.com', age: 35 },
-  { id: '2', name: 'Steve Smith', email: 'ssmith@example.com', age: 25 },
-  { id: '3', name: 'Sara Williams', email: 'swilliams@example.com', age: 32 }
-]
 
 const CustomerType = new GraphQLObjectType({
   name: 'Customer',
@@ -33,15 +27,15 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve (parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id === args.id) return customers[i];
-        }
+        return axios.get(`http://localhost:3000/customers/${args.id}`)
+          .then(response => response.data)
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve (parentValue, args) {
-        return customers
+        return axios.get('http://localhost:3000/customers/')
+          .then(response => response.data)
       }
     }
   }
