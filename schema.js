716367359ca37e204a218ed88d40af5b92ffc131
Graham Swan <thinkswan@gmail.com>
2019-02-22 20:42:22 +0100

    Add CustomerType, root query, and seed data

--- schema.js
+++ schema.js
@@ -7,6 +7,40 @@ const {
   GraphQLNonNull
 } = require('graphql')
 
-module.exports = new GraphQLSchema({
+// Seed data
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@example.com', age: 35 },
+  { id: '2', name: 'Steve Smith', email: 'ssmith@example.com', age: 25 },
+  { id: '3', name: 'Sara Williams', email: 'swilliams@example.com', age: 32 }
+]
 
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve (parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) return customers[i];
+        }
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
