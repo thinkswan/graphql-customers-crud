32682a434ac0ea2ef6684c248e1751eca3ff8a8a
Graham Swan <thinkswan@gmail.com>
2019-02-22 20:32:29 +0100

    Add /graphql route to server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema.js')
 
 const PORT = 4000
 const app = express()
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}))
 
 app.listen(PORT, () => {
   console.log('Server is running on port 4000');
